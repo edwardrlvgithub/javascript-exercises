d8ad3d9464dfb9e6a7c8c7304246936066e2810c
Edward Raphael Viñegas <edwardraphael1014@gmail.com>
2023-11-22 14:45:52 +0800

    finished exercised 1-4 (removed .skip in js tests)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function (string, num_times) {
+  let repeatedString = "";
 
+  for (let i = 0; i < num_times; i++) {
+    repeatedString += string;
+  }
+  if (num_times < 0) {
+    return "ERROR";
+  }
+  return repeatedString;
 };
+
+console.log(repeatString("hey", 1));
 
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...itemsToRemove) {
+  let modifiedArray = array.filter((item) => !itemsToRemove.includes(item));
+  return modifiedArray;
 };
-
+console.log(removeFromArray([1, 2, 3, 4], 1, 4));
 // Do not edit below this line
 module.exports = removeFromArray;
